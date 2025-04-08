f7ed9959e2293d093c430a4dc27d2c5fbe9e6c37
dafcorg <75268323+dafcorg@users.noreply.github.com>
2025-04-08 13:03:36 +0200

    Update conectando.py

--- conectando.py
+++ conectando.py
@@ -12,16 +12,12 @@
 #____
 
 from fastapi import FastAPI, UploadFile, File
-import nest_asyncio
 import uvicorn
 from PIL import Image
 import io
 import requests
 import time
 
-
-# Parche para permitir async en Colab
-nest_asyncio.apply()
 
 # Crear app de FastAPI
 app = FastAPI()
@@ -46,25 +42,4 @@ nest_asyncio.apply()
 public_url = ngrok.connect(8000)
 print(f"🚀 Tu API está en: {public_url}/predict")
 uvicorn.run(app, host="0.0.0.0", port=8000)
-
-#------------------------
-#################################
-#CLIENTE
-#################################
-# Otra ventana
-
-import requests
-
-url = "URL/predict" 
-
-img_url = "https://raw.githubusercontent.com/dafcorg/servimos/refs/heads/main/img/YellowLabradorLooking_new.jpg"
-img_bytes = requests.get(img_url).content
-
-response = requests.post(
-    url,
-    files={"file": ("dog.jpg", img_bytes, "image/jpeg")}
-)
-
-print(response.status_code)
-print(response.json())
 
